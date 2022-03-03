fcd506d4e65489c56515d5061f2a1f59c69288c7
Jack-24265241 <24265241@edgehill.ac.uk>
2022-03-03 18:45:16 +0000

    PT5 Complete

--- Portfolio Tasks/PortfolioTask5/PortfolioTask5.java
+++ Portfolio Tasks/PortfolioTask5/PortfolioTask5.java
@@ -2,7 +2,7 @@
 /**
  * Class to address all exercises for Portfolio Task 5
  * 
- * last updated: 23/2/22
+ * last updated: 3/3/22
  * 
  * @author Jack Smith - 24265241
  *
@@ -11,9 +11,7 @@ public class PortfolioTask5 {
 
 	
 	public static void main(String[] args) {
-		
-		exercise2();
-		
+		exercise3(); // can be changed to display the relevant exercise
 	}
 	
 	/**
@@ -53,54 +51,131 @@ public class PortfolioTask5 {
 		StdDraw3D.finished();
 	}
 	
-	private static void moveAroundPoint(StdDraw3D.Shape shape, StdDraw3D.Vector3D origin, double radius, double angle, char axis, boolean invertDirection) {
+	/**
+	 * Allows a shape to rotate around a given point
+	 * 
+	 * @param shape the shape that will rotate around the point
+	 * @param origin the point for the shape to rotate around
+	 * @param radius the radius of the rotation circle
+	 * @param angle the angle to rotate by
+	 * @param axis the axis to rotate around
+	 */
+	private static void rotateAroundPoint(StdDraw3D.Shape shape, StdDraw3D.Vector3D origin, double radius, double angle, char axis) {
 		
-		StdDraw3D.Vector3D newPosition = null;
-		int inverter = invertDirection ? -1 : 1;
+		StdDraw3D.Vector3D newPosition = null; // intitalise new position vector
 		
-		switch(axis) {
+		switch(axis) { // switch each axis
 		case 'x':
-			newPosition = new StdDraw3D.Vector3D(origin.x, origin.y + radius * Math.cos(angle), origin.z + radius * Math.sin(angle) * inverter);
+			newPosition = new StdDraw3D.Vector3D(origin.x, origin.y + radius * Math.cos(angle), origin.z + radius * Math.sin(angle));
 			break;
 		case 'y':
-			newPosition = new StdDraw3D.Vector3D(origin.x + radius * Math.cos(angle), origin.y, origin.z + radius * Math.sin(angle) * inverter);
+			newPosition = new StdDraw3D.Vector3D(origin.x + radius * Math.cos(angle), origin.y, origin.z + radius * Math.sin(angle));
 			break;
 		case 'z':
-			newPosition = new StdDraw3D.Vector3D(origin.x + radius * Math.cos(angle), origin.y + radius * Math.sin(angle) * inverter, origin.z);
+			newPosition = new StdDraw3D.Vector3D(origin.x + radius * Math.cos(angle), origin.y + radius * Math.sin(angle), origin.z);
 			break;
 		}
 		
-		if(newPosition != null) {
-			shape.move(newPosition.minus(shape.getPosition()));
+		if(newPosition != null) { // if no valid position, do not move
+			shape.move(newPosition.minus(shape.getPosition())); // move by the difference between the new position and the current positon
 		}
 	}
 	
+	/**
+	 * Method for displaying the solution to Exercise 2.
+	 * Draws a rotating sun and 
+	 */
 	public static void exercise2() {
 		// set the scale
 		StdDraw3D.setScale(-10, 10);
 		
 		// sun
-		StdDraw3D.Shape sun = StdDraw3D.wireSphere(0, 0, 0, 3);
+		StdDraw3D.setPenColor(StdDraw3D.ORANGE);
+		StdDraw3D.Shape sun = StdDraw3D.wireSphere(0, 0, 0, 4);
 		
 		// planet
+		StdDraw3D.setPenColor(StdDraw3D.BLUE);
 		StdDraw3D.Shape planet = StdDraw3D.sphere(7, 0, 0, 1);
 		
-		double angle = 0;
+		double angle = 0; // planets current orbit around the sun
+		double planetOrbitSpeed = 0.02;
 		
+		double sunRotationSpeed = 0.5;
+		double planetRotationSpeed = 0.2;
+		
+		// animation loop
 		while(true) {
 			// rotate the sun
-			sun.rotate(0, 0, 0.3);
+			sun.rotate(0, 0, -sunRotationSpeed); // negative for clockwise
 			
 			// move the planet
-			moveAroundPoint(planet, new StdDraw3D.Vector3D(0, 0, 0), 7, angle, 'z', false);
-			angle += 0.02; // higher value => higher speed of orbit
+			rotateAroundPoint(planet, new StdDraw3D.Vector3D(0, 0, 0), 12, angle, 'z');
+			angle -= planetOrbitSpeed; // higher value => higher speed of orbit
+			if(angle >= 2 * Math.PI) { // prevent angle continuously growing
+				angle = 0;
+			}
 			
 			// rotate the planet
-			planet.rotate(0, 0, 0.1);
+			planet.rotate(0, 0, -planetRotationSpeed); // negative for clockwise
 			
 			// show the frame (~60fps)
 			StdDraw3D.show(17);
 		}
+	}
+	
+	public static void exercise3() {
+		// set the scale
+		StdDraw3D.setScale(-10, 10);
 		
+		// sun
+		StdDraw3D.setPenColor(StdDraw3D.ORANGE);
+		StdDraw3D.Shape sun = StdDraw3D.wireSphere(0, 0, 0, 4);
+		
+		// planet
+		StdDraw3D.setPenColor(StdDraw3D.BLUE);
+		StdDraw3D.Shape planet = StdDraw3D.sphere(7, 0, 0, 1);
+		
+		double angle = 0; // planets current orbit around the sun
+		double planetOrbitSpeed = 0.02;
+		
+		double sunRotationSpeed = 0.5;
+		double planetRotationSpeed = 0.2;
+		
+		boolean clockwise = true;
+		
+		// animation loop
+		while(true) {
+			
+			if(StdDraw3D.mouse3Pressed()) { // when middle mouse button is pressed
+				StdDraw3D.finished(); // end animation
+			}
+			if(StdDraw3D.mouse1Pressed()) { // when left mouse button is pressed
+				clockwise = false; // go anti-clockwise
+			} else if(StdDraw3D.mouse2Pressed()) { // when right mouse button is pressed
+				clockwise = true; // go clockwise
+			}
+			
+			if(clockwise) {
+				angle -= planetOrbitSpeed;
+			} else {
+				angle += planetOrbitSpeed;
+			}
+			
+			// rotate the sun
+			sun.rotate(0, 0, -sunRotationSpeed); // negative for clockwise
+
+			// move the planet
+			rotateAroundPoint(planet, new StdDraw3D.Vector3D(0, 0, 0), 12, angle, 'z');
+			
+			if(angle >= 2 * Math.PI || angle <= -2 * Math.PI) { // prevent angle continuously growing
+				angle = 0;
+			}
+
+			// rotate the planet
+			planet.rotate(0, 0, -planetRotationSpeed); // negative for clockwise
+
+			// show the frame (~60fps)
+			StdDraw3D.show(17);
+		}
 	}
 }
